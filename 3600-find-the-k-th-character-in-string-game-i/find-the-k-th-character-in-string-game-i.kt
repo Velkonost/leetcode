8f60df70784c8ab228c38ebe87fe91efc51edc89
Artem Klimenko <klimenkocpp@icloud.com>
2025-07-03 13:47:40 +0600

    Time: 14 ms (55.56%) | Memory: 46.3 MB (22.22%) - LeetSync

---
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun kthCharacter(k: Int): Char {
+        var word = "a"
+        while(word.length < k) {
+            var updatedWord = word
+            for (char in word) {
+                val nextChar = char + 1
+                updatedWord += nextChar
+            }
+            word = updatedWord
+        }
+        return word[k - 1]
+    }
+}
