c09ceeabb2d0421082e6a2315d03cf5d26ba8491
Artem Klimenko <klimenkocpp@icloud.com>
2025-07-16 13:35:49 +0600

    Time: 5 ms (100.00%) | Memory: 68.3 MB (100.00%) - LeetSync

---
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.kt
@@ -0,0 +1,24 @@
+class Solution {
+    // Complexity:
+    // Time O(N) and Space O(1) where N is the length of nums.
+    fun maximumLength(nums: IntArray): Int {
+        // The possible patterns include all odds, all evens,
+        // alternating odd-even, and alternating even-odd. For the
+        // alternating patterns, we only consider the one that starts
+        // with the parity of nums[0], since it is longer than the
+        // other by one.
+        var cntEvens = 0
+        var alterLen = 0
+        var alterBit = nums[0] and 1
+        for (num in nums) {
+            if (num and 1 == 0) {
+                cntEvens++
+            }
+            if (num and 1 == alterBit) {
+                alterLen++
+                alterBit = alterBit xor 1
+            }
+        }
+        return maxOf(cntEvens, nums.size - cntEvens, alterLen)
+    }
+}
