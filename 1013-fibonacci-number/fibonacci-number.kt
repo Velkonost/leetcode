71f01be0be2d0d2540b2ffe9f438624b0b00c696
Artem Klimenko <klimenkocpp@icloud.com>
2025-07-23 14:23:08 +0300

    Time: 88 ms (10.37%) | Memory: 38.6 MB (97.53%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.kt
+++ 1013-fibonacci-number/fibonacci-number.kt
@@ -1,19 +1,10 @@
 class Solution {
     fun fib(n: Int): Int {
-        var first = 0
-        var second = 1
+        
         if (n == 0) return 0
         if (n == 1) return 1
+        if (n == 2) return 1
 
-        var num = 2
-        var fib = first + second
-        while (num != n + 1) {
-            fib = first + second
-            first = second
-            second = fib
-            num ++
-        }
-
-        return fib
+        return fib(n - 1) + fib(n - 2)
     }
 }
