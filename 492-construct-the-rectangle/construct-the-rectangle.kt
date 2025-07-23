8a2be6ac88c31f1da6121f0979ad2ea56b07b20d
Artem Klimenko <klimenkocpp@icloud.com>
2025-07-23 13:56:08 +0300

    Time: 0 ms (100.00%) | Memory: 40.6 MB (9.09%) - LeetSync

--- 492-construct-the-rectangle/construct-the-rectangle.kt
+++ 492-construct-the-rectangle/construct-the-rectangle.kt
@@ -1,21 +1,10 @@
 class Solution {
     fun constructRectangle(area: Int): IntArray {
-        var minDist = Integer.MAX_VALUE
-        var length = area
-        var width = 1
-        val half = area / 2
-        for (i in 1..area) {
-            var nL = i
-            if (area % nL == 0) {
-                var nW = area / nL
-                if (nL >= nW && abs(nL - nW) < minDist) {
-                    minDist = abs(nL - nW)
-                    width = nW
-                    length = nL
-                }
-            }
+        var width = sqrt(area.toDouble()).toInt()
+        while(area % width != 0) {
+            width --
         }
-
+        val length = area / width
         return intArrayOf(length, width)
     }
 }
